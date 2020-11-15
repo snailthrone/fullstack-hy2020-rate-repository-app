9928e37d8235486292f3d91240cb91b7e9886a75
Eemeli Martti <eemeli.martti@live.fi>
2020-11-15 16:00:35 +0200

    REPOSITORIES query supports searchKeyword parameter

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -3,15 +3,28 @@ import { useEffect, useState } from 'react';
 
 import { REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = variables => {
+const checkParameters = object => {
+  try {
+    return Object.keys(object)
+      .filter(key => !!object[key])
+      .map(key => ({ [key]: object[key] }))
+      .reduce((previousValue, currentValue) => ({ ...previousValue, ...currentValue }), {});
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const useRepositories = (orderBy, orderDirection, searchKeyword) => {
   const [getRepositories, { data, loading }] = useLazyQuery(REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
   const [repositories, setRepositories] = useState();
 
   useEffect(() => {
-    getRepositories({ variables });
-  }, [variables]);
+    getRepositories({
+      variables: { orderBy, orderDirection, searchKeyword },
+    });
+  }, [orderBy, orderDirection, searchKeyword]);
 
   useEffect(() => {
     if (data) {
