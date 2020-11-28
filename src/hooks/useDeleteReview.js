fb6da68a0c19ff46b3960a8cbfa82236dbc3f2fc
Eemeli Martti <eemeli.martti@live.fi>
2020-11-28 20:37:06 +0200

    created useDeleteReview hook

---
+++ src/hooks/useDeleteReview.js
@@ -0,0 +1,18 @@
+import { useMutation } from '@apollo/react-hooks';
+import { DELETE_REVIEW } from '../graphql/mutations';
+
+const useDeleteReview = () => {
+  const [mutate, { data }] = useMutation(DELETE_REVIEW);
+
+  const deleteReview = async variables => {
+    try {
+      await mutate({ variables });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  return [deleteReview];
+};
+
+export default useDeleteReview;
