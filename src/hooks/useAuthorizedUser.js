cc96d59666847f9306259eed7934fbf794c9f1d4
Eemeli Martti <eemeli.martti@live.fi>
2020-11-28 20:44:13 +0200

    added fetchMore functionality

--- src/hooks/useAuthorizedUser.js
+++ src/hooks/useAuthorizedUser.js
@@ -2,9 +2,44 @@ import { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { AUTHORIZED_USER } from '../graphql/queries';
 
-const useAuthorizedUser = () => {
+const useAuthorizedUser = (includeReviews = false, first = 3) => {
   const [authorized, setAuthorized] = useState(false);
-  const { data } = useQuery(AUTHORIZED_USER, { fetchPolicy: 'cache-and-network' });
+  const { data, loading, fetchMore, refetch } = useQuery(AUTHORIZED_USER, {
+    variables: { first, includeReviews },
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const handleFetchMore = () => {
+    const canFetchMore = !loading && data && data.authorizedUser.reviews.pageInfo.hasNextPage;
+
+    if (!canFetchMore) {
+      return;
+    }
+
+    fetchMore({
+      query: AUTHORIZED_USER,
+      variables: {
+        after: data.authorizedUser.reviews.pageInfo.endCursor,
+        first,
+        includeReviews,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const nextResult = {
+          authorizedUser: {
+            ...previousResult.authorizedUser,
+            reviews: {
+              ...fetchMoreResult.authorizedUser.reviews,
+              edges: [
+                ...previousResult.authorizedUser.reviews.edges,
+                ...fetchMoreResult.authorizedUser.reviews.edges,
+              ],
+            },
+          },
+        };
+        return nextResult;
+      },
+    });
+  };
 
   useEffect(() => {
     if (data) {
@@ -17,7 +52,7 @@ const useAuthorizedUser = () => {
     }
   }, [data]);
 
-  return authorized;
+  return [authorized, data, handleFetchMore, refetch];
 };
 
 export default useAuthorizedUser;
